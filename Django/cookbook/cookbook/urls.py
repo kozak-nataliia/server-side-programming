7278198bf3aac7ad8e1da41fa4145e5b48b1cda1
Nataliia Kozak <nataliia.kozak.shi.2024@lpnu.ua>
2025-11-13 20:37:28 +0200

    feat(api): register CRUD endpoints in router

--- Django/cookbook/cookbook/urls.py
+++ Django/cookbook/cookbook/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipes.API.viewsets import (
+    IngredientViewSet, RecipeViewSet,
+    IngredientCategoryViewSet, RecipeCategoryViewSet,
+    UnitViewSet, RecipeItemViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'ingredients', IngredientViewSet, basename='ingredient')
+router.register(r'recipes', RecipeViewSet, basename='recipe')
+router.register(r'ingredient-categories', IngredientCategoryViewSet, basename='ingredient-category')
+router.register(r'recipe-categories', RecipeCategoryViewSet, basename='recipe-category')
+router.register(r'units', UnitViewSet, basename='unit')
+router.register(r'items', RecipeItemViewSet, basename='recipe-item')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
